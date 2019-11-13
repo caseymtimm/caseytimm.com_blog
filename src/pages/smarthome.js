f3a7c854c9c14e239982864e3934058785b63202
Casey Timm <casey.m.timm@gmail.com>
2019-11-13 18:50:30 -0500

    Added thermostat

--- src/pages/smarthome.js
+++ src/pages/smarthome.js
@@ -12,14 +12,50 @@ const Smarthome = () => {
     count: "loading",
   })
 
+  let hvac = useFetch("http://cms.caseytimm.com/hubitat/hvac", {
+    "DeviceWatch-DeviceStatus": "loading",
+    coolingSetpoint: "loading",
+    deviceAlive: "loading",
+    deviceTemperatureUnit: "F",
+    heatingSetpoint: "loading",
+    humidity: "loading",
+    maxCoolingSetpoint: "loading",
+    maxHeatingSetpoint: "loading",
+    minCoolingSetpoint: "loading",
+    minHeatingSetpoint: "loading",
+    resumeProgram: "loading",
+    schedule: "loading",
+    supportedThermostatFanModes: "loading",
+    supportedThermostatModes: "loading",
+    temperature: "loading",
+    thermostat: "loading",
+    thermostatFanMode: "loading",
+    thermostatMode: "loading",
+    thermostatOperatingState: "loading",
+    thermostatSetpoint: "loading",
+  })
+
   return (
     <Layout>
       <Typography variant="h1">My Smart Home</Typography>
       <Container style={{ marginTop: "20px", marginBottom: "20px" }}>
         <Divider />
       </Container>
+      <Typography variant="h2">Lights</Typography>
       <Typography>
         Currently I have {lights.count} lights and {lightson.count} are on!
+      </Typography>
+      <Typography variant="h2">Thermostat</Typography>
+      <Typography>
+        My thermostat is set to {hvac.thermostatMode} with a setpoint of{" "}
+        {hvac.thermostatMode === "heat"
+          ? hvac.heatingSetpoint
+          : hvac.coolingSetpoint}
+        °{hvac.deviceTemperatureUnit}
+      </Typography>
+      <Typography>
+        It is currently {hvac.temperature}°{hvac.deviceTemperatureUnit} with a
+        relative humidity of {hvac.humidity}%
       </Typography>
     </Layout>
   )
