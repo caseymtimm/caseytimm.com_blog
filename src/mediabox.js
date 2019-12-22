e8060f454404f37aa01d10b22c76d17c2df24749
Casey Timm <casey.m.timm@gmail.com>
2019-12-22 18:44:58 -0500

    Broke out media box and added logic for pdf pages

---
+++ src/mediabox.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react"
+import { Document, Page, pdfjs } from "react-pdf"
+import { useTheme } from "@material-ui/styles"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+import { Grid, Button } from "@material-ui/core"
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+function useWidth() {
+  const theme = useTheme()
+  const keys = [...theme.breakpoints.keys].reverse()
+  return (
+    keys.reduce((output, key) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const matches = useMediaQuery(theme.breakpoints.up(key))
+      return !output && matches ? key : output
+    }, null) || "xs"
+  )
+}
+
+export default function Image(props) {
+  const width = useWidth()
+  const [pages, setPages] = useState(1)
+  const [page, setPage] = useState(1)
+
+  console.log(props.src)
+
+  return props.src
+    .split(/\#|\?/)[0]
+    .split(".")
+    .pop()
+    .trim()
+    .toLowerCase() === "pdf" ? (
+    <>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+          <Document
+            file={props.src}
+            onLoadSuccess={({ numPages }) => setPages(numPages)}
+          >
+            <Page
+              pageNumber={page}
+              width={
+                width === "xl"
+                  ? 1000
+                  : width === "lg"
+                  ? 900
+                  : width === "md"
+                  ? 500
+                  : width === "sm"
+                  ? 400
+                  : 200
+              }
+            ></Page>
+          </Document>
+        </Grid>
+        <Grid item>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item>
+              {page > 1 ? (
+                <Button onClick={() => setPage(page - 1)}>Previous Page</Button>
+              ) : (
+                <></>
+              )}
+            </Grid>
+            <Grid item>
+              Page {page} of {pages}
+            </Grid>
+            <Grid item>
+              {page < pages ? (
+                <Button onClick={() => setPage(page + 1)}>Next Page</Button>
+              ) : (
+                <></>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  ) : (
+    <img
+      alt="Markdown"
+      {...props}
+      style={{
+        maxWidth: "50%",
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    />
+  )
+}
--- src/post.js
+++ src/post.js
@@ -1,14 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "@reach/router";
-import ReactMarkdown from "react-markdown";
-import { Typography, Container, Grid, Button } from "@material-ui/core";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
-import { Document, Page, pdfjs } from "react-pdf";
-import { ThemeProvider, useTheme, createMuiTheme } from "@material-ui/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React, { useEffect } from "react"
+import { Link } from "@reach/router"
+import ReactMarkdown from "react-markdown"
+import { Typography, Container } from "@material-ui/core"
+import { gql } from "apollo-boost"
+import { useQuery } from "@apollo/react-hooks"
+import MediaBox from "./mediabox"
 
 const POST = gql`
   query Post($post: ID!) {
@@ -27,89 +23,12 @@ const POST = gql`
       updated_at
     }
   }
-`;
-
-function useWidth() {
-  const theme = useTheme();
-  const keys = [...theme.breakpoints.keys].reverse();
-  return (
-    keys.reduce((output, key) => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const matches = useMediaQuery(theme.breakpoints.up(key));
-      return !output && matches ? key : output;
-    }, null) || "xs"
-  );
-}
-
-function Image(props) {
-  const width = useWidth();
-  const [pages, setPages] = useState(1);
-  const [page, setPage] = useState(1);
-
-  console.log(props.src);
-
-  return props.src
-    .split(/\#|\?/)[0]
-    .split(".")
-    .pop()
-    .trim()
-    .toLowerCase() === "pdf" ? (
-    <>
-      <Grid container direction="column" justify="center" alignItems="center">
-        <Grid item>
-          <Document
-            file={props.src}
-            onLoadSuccess={({ numPages }) => setPages(numPages)}
-          >
-            <Page
-              pageNumber={1}
-              width={
-                width === "xl"
-                  ? 1000
-                  : width === "lg"
-                  ? 900
-                  : width === "md"
-                  ? 500
-                  : width === "sm"
-                  ? 400
-                  : 200
-              }
-            ></Page>
-          </Document>
-        </Grid>
-        <Grid item>
-          <Grid container direction="row" justify="center" alignItems="center">
-            <Grid item>
-              {page > 1 ? <Button>Previous Page</Button> : <></>}
-            </Grid>
-            <Grid item>
-              Page {page} of {pages}
-            </Grid>
-            <Grid item>
-              {page < pages ? <Button>Next Page</Button> : <></>}
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </>
-  ) : (
-    <img
-      alt="Markdown"
-      {...props}
-      style={{
-        maxWidth: "50%",
-        display: "block",
-        marginLeft: "auto",
-        marginRight: "auto"
-      }}
-    />
-  );
-}
+`
 
 const Post = ({ id, setImage }) => {
   const { loading, error, data } = useQuery(POST, {
-    variables: { post: id }
-  });
+    variables: { post: id },
+  })
 
   useEffect(() => {
     if (data)
@@ -117,11 +36,11 @@ const Post = ({ id, setImage }) => {
         data.Image !== "undefined"
           ? `https://cms.caseytimm.com${data.post.Image.url}`
           : undefined
-      );
-  }, [data]);
+      )
+  }, [data])
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
 
   return (
     <>
@@ -151,13 +70,13 @@ const Post = ({ id, setImage }) => {
         <ReactMarkdown
           source={data.post.Content}
           escapeHtml={false}
-          renderers={{ image: Image }}
+          renderers={{ image: MediaBox }}
         />
         <br />
       </Container>
       {/*</Layout>*/}
     </>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
